bac9309a8062e82bf510cce8574c78232098316e
anirudhlohia4 <anirudh@Homes-MacBook-Pro.local>
2015-01-24 17:22:15 +0100

    Assignment2 code
    Wrote the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+#makeCacheMatrix: This function stores a matrix and its inverse. 
+#The inverse matric is not computed within the function but has to calculated from
+#outside and set.It is assumed that matrix is invertible one.
+#ignore function get.env, it is for debugging purpose
 
-## Write a short comment describing this function
+#cacheSolve: This function takes makeCacheMatrix function as its parameter. It 
+#computes inverse mtrix and sets it if not already set else it fetches it from cache
 
-makeCacheMatrix <- function(x = matrix()) {
+#below are sample commands to run the function
+#mt <- matrix(c(2,3,2,2),2,2)
+#source("cachematrix.R")
+#cacheM <- makeCacheMatrix(mt)
+#ls(parent.env(cacheM$get.env()))
+#parent.env(cacheM$get.env())$mx
+#parent.env(cacheM$get.env())$invt.mx
+#cacheSolve(cacheM)
+#parent.env(cacheM$get.env())$invt.mx
 
+makeCacheMatrix <- function(mx=matrix()){ 
+    invt.mx <- NULL 
+    
+    get <- function() mx 
+
+    get.invt.mx <- function() invt.mx
+    
+    set.invt.mx <- function(invt.m=matrix()) invt.mx <<- invt.m
+      
+    get.env  <- function() environment()
+    
+    list(get=get,get.invt.mx=get.invt.mx,set=set,get.env=get.env)    
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cacheM,...){
+    if(is.null(cacheM$get.invt.mx())){
+        print("Inverse not found, setting inverse")
+        cacheM$set(solve(cacheM$get()))
+    }else{
+        print("Inverse fetched from memory")
+    }
+    cacheM$get.invt.mx()
 }
