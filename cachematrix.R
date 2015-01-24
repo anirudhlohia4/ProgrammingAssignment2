9c9e6ca40c3766f878f0272623c8ecc05d613c64
anirudhlohia4 <anirudh@Homes-MacBook-Pro.local>
2015-01-24 21:50:36 +0100

    Improved the comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,3 @@
-#makeCacheMatrix: This function stores a matrix and its inverse. 
-#The inverse matric is not computed within the function but has to calculated from
-#outside and set.It is assumed that matrix is invertible one.
-#ignore function get.env, it is for debugging purpose
-
-#cacheSolve: This function takes makeCacheMatrix function as its parameter. It 
-#computes inverse mtrix and sets it if not already set else it fetches it from cache
-
 #below are sample commands to run the function
 #mt <- matrix(c(2,3,2,2),2,2)
 #source("cachematrix.R")
@@ -16,21 +8,35 @@
 #cacheSolve(cacheM)
 #parent.env(cacheM$get.env())$invt.mx
 
+#makeCacheMatrix: This function stores a matrix and its inverse. 
+#The inverse matric is not computed within the function but has to calculated from
+#outside and set.It is assumed that matrix is invertible one.
+#ignore function get.env, it is for debugging purpose
 makeCacheMatrix <- function(mx=matrix()){ 
+	#inverse matrix is initialized to null
     invt.mx <- NULL 
     
+    #get() returns the matrix used in initialization
     get <- function() mx 
-
+	
+	#get.invt.mx() returns the inverse matrix if it exists else NULL
     get.invt.mx <- function() invt.mx
     
+    #set.invt.mx() sets the inverse matrix
     set.invt.mx <- function(invt.m=matrix()) invt.mx <<- invt.m
-      
+    
+    #get.env() used for understading the environment, not of use
     get.env  <- function() environment()
     
+    #returns list of functions
     list(get=get,get.invt.mx=get.invt.mx,set=set,get.env=get.env)    
 }
 
+#cacheSolve: This function takes makeCacheMatrix function as its parameter. It 
+#computes inverse mtrix and sets it if not already set else it fetches it from cache
 cacheSolve <- function(cacheM,...){
+	
+	#check inverse matrix exist or not
     if(is.null(cacheM$get.invt.mx())){
         print("Inverse not found, setting inverse")
         cacheM$set(solve(cacheM$get()))
